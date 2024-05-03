5884f914346773929fac6f065a121c809af204f4
IESTomasAraujo <tomasaraujo1999@hotmail.com>
2024-05-02 23:54:19 -0300

    Codesmell: Duplicated Code - Mod GetMontoExtraido

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -27,31 +27,31 @@ public class Cuenta {
   }
 
   public void poner(double cuanto) {
-    if (cuanto <= 0) {
-      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
-    }
+    if (cuanto <= 0) {// Código duplicado
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");// Código duplicado
+    }// Código duplicado
 
     if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
-    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
+    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);// Código duplicado
   }
 
   public void sacar(double cuanto) {
-    if (cuanto <= 0) {
-      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
-    }
+    if (cuanto <= 0) {// Código duplicado
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");// Código duplicado
+    }// Código duplicado
     if (getSaldo() - cuanto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
-    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+    double montoExtraidoHoy = getMontoExtraidoA();
     double limite = 1000 - montoExtraidoHoy;
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
+    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);// Código duplicado
   }
 
   // ANTES -> public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
@@ -61,9 +61,9 @@ public class Cuenta {
     movimientos.add(movimiento);
   }
 
-  public double getMontoExtraidoA(LocalDate fecha) {
+  public double getMontoExtraidoA() {// No es necesario el parámetro, se podría instanciar sin parametro y se calcularía con la fecha de hoy igualmente
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(LocalDate.now()))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
